725e6d6e8ae0d819268397f4f0fb17a5fc8343e6
DennisRono <finnneron1@gmail.com>
2023-04-04 20:21:30 +0300

    custom stylings ⚡

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import '../styles/css/landing.css'
+import { Link } from 'react-router-dom'
 import Header from './Header'
 import MadDesigner from '../assets/images/mad-designer.png'
 
@@ -11,11 +12,16 @@ const Landing = () => {
         <div className="landingWrapper">
           <div className="landingContainer">
             <div className="landingtexts">
-              <h1 className="ct_lnd_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, dolorem.</h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, aliquid aut ducimus temporibus ratione debitis earum molestiae commodi cupiditate. Excepturi.</p>
+              <div className="lndingtextswrapper">
+                <h1 className="ct_lnd_text">Lorem ipsum dolor sit amet consectetur adipisicing.</h1>
+                <p className="ct_lnd_text_p">Our purpose is people™ and we provide HR, payroll, and workforce management technology and services that inspire your people and elevate the work experience.</p>
+                <Link to="/contact" className="cta_get_started">get started</Link>
+              </div>
             </div>
             <div className="landingImage">
-              <img src={MadDesigner} alt="" />
+              <div className="lndingImageWrapper">
+                <img src={MadDesigner} alt="" />
+              </div>
             </div>
           </div>
         </div>
