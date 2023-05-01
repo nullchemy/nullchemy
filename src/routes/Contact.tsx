e4343f6b072676da96b941101d24b589f8d93b97
DennisRono <finnneron1@gmail.com>
2023-05-01 13:12:50 +0300

    started building the contact us page

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,14 +1,52 @@
 import React, { Fragment } from 'react'
+import '../styles/css/contact.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Maintainance from '../components/Maintainance'
 
 const Contact = () => {
   return (
     <Fragment>
-        <Header />
-        <Maintainance />
-        <Footer />
+      <Header />
+      <div className="contact">
+        <div className="contactContainer">
+          <div className="contactWrapper">
+            <h2 className="contHeader">love to hear from you,</h2>
+            <h2 className="contHeader">get in touch</h2>
+            <div className="contform">
+              <form className="contactForm">
+                <div className="contflex">
+                  <div className="contformgroup">
+                    <label htmlFor="your name">
+                      Your Name <span>*</span>
+                    </label>
+                    <br />
+                    <input type="text" className="continput" />
+                  </div>
+                  <div className="contformgroup">
+                    <label htmlFor="your name">
+                      Your Email <span>*</span>
+                    </label>
+                    <br />
+                    <input type="email" className="continput" />
+                  </div>
+                </div>
+                <div className="contformgroup">
+                  <label htmlFor="your name">
+                    Message <span>*</span>
+                  </label>
+                  <br />
+                  <textarea className="conttextarea"></textarea>
+                </div>
+                <button type="submit" className="contsendbutton">
+                  send
+                </button>
+              </form>
+              <div className="contInfoBox"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </Fragment>
   )
 }
