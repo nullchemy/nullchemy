57501b7c284f15598bbb4582bbe358014f08e7e9
DennisRono <finnneron1@gmail.com>
2023-05-01 13:46:46 +0300

    finished ui/ux of contact page

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -2,6 +2,9 @@ import React, { Fragment } from 'react'
 import '../styles/css/contact.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import { ReactComponent as Envelop } from '../assets/svg/envelope.svg'
+import { ReactComponent as Phone } from '../assets/svg/phone.svg'
+import { ReactComponent as Location } from '../assets/svg/location.svg'
 
 const Contact = () => {
   return (
@@ -32,6 +35,13 @@ const Contact = () => {
                 </div>
                 <div className="contformgroup">
                   <label htmlFor="your name">
+                    Subject <span>*</span>
+                  </label>
+                  <br />
+                  <input type="text" className="continput" />
+                </div>
+                <div className="contformgroup">
+                  <label htmlFor="your name">
                     Message <span>*</span>
                   </label>
                   <br />
@@ -41,7 +51,23 @@ const Contact = () => {
                   send
                 </button>
               </form>
-              <div className="contInfoBox"></div>
+              <div className="contInfoBox">
+                <h3>contact information</h3>
+                <div className="continfodirect">
+                  <div className="continfoTouch">
+                    <Phone className="contInfoIc" />
+                    <span>+254798116710</span>
+                  </div>
+                  <div className="continfoTouch">
+                    <Envelop className="contInfoIc" />
+                    <span>contact@nullchemy.com</span>
+                  </div>
+                  <div className="continfoTouch">
+                    <Location className="contInfoIc" />
+                    <span>102 Street Nairobi</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
