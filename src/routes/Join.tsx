0325200250883d889d3461e3a5f52c800875ca30
DennisRono <finnneron1@gmail.com>
2023-05-06 10:53:19 +0300

    worked of join us page UI

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -12,7 +12,16 @@ const Join = () => {
           <div className="jgwrapper">
             <h2 className="jgTitle">Join us on Github</h2>
             <form className="jgform">
-              <div className="jgformGroup"></div>
+              <div className="jgformGroup">
+                <label htmlFor="username">
+                  Your Github username <span>*</span>
+                </label>
+                <br />
+                <input type="text" className="jginputTextInput" />
+              </div>
+              <button type="submit" className="jgRequestBtn">
+                Request
+              </button>
             </form>
           </div>
         </div>
