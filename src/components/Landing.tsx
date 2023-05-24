3e25e3e8ce5f5605a8d05350d57d063ae2f13fa9
DennisRono <finnneron1@gmail.com>
2023-05-24 11:18:00 +0300

    added partners in landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,6 +3,8 @@ import '../styles/css/landing.css'
 import { Link } from 'react-router-dom'
 import Header from './Header'
 import MadDesigner from '../assets/images/hero-bg.png'
+import DennisKibet from '../assets/images/dennis_kibet_logo.png'
+import TenAfric from '../assets/images/tenafric.png'
 
 const Landing = () => {
   return (
@@ -11,28 +13,36 @@ const Landing = () => {
         <Header />
         <div className="landingWrapper">
           <div className="landingContainer">
-            <div className="landingtexts">
-              <div className="lndingtextswrapper">
-                <h1 className="ct_lnd_text">
-                  {/* Revolutionize your business with nullchemy's innovative
+            <div className="landingTop">
+              <div className="landingtexts">
+                <div className="lndingtextswrapper">
+                  <h1 className="ct_lnd_text">
+                    {/* Revolutionize your business with nullchemy's innovative
                   software solutions today! */}
-                  Transforming ideas into a digital reality.
-                </h1>
-                <p className="ct_lnd_text_p">
-                  At nullchemy, we are dedicated to providing our clients with
-                  high-quality software development services that are tailored
-                  to meet their unique needs. Our team of expert developers work
-                  tirelessly to ensure that your project is completed on time,
-                  within budget, and to your exact specifications.
-                </p>
-                <Link to="/contact" className="cta_get_started">
-                  get started
-                </Link>
+                    Transforming ideas into a digital reality.
+                  </h1>
+                  <p className="ct_lnd_text_p">
+                    At nullchemy, we are dedicated to providing our clients with
+                    high-quality software development services that are tailored
+                    to meet their unique needs. Our team of expert developers
+                    work tirelessly to ensure that your project is completed on
+                    time, within budget, and to your exact specifications.
+                  </p>
+                  <Link to="/contact" className="cta_get_started">
+                    get started
+                  </Link>
+                </div>
+              </div>
+              <div className="landingImage">
+                <div className="lndingImageWrapper">
+                  <img src={MadDesigner} title="mad developer" alt="" />
+                </div>
               </div>
             </div>
-            <div className="landingImage">
-              <div className="lndingImageWrapper">
-                <img src={MadDesigner} title="mad developer" alt="" />
+            <div className="landingBottom">
+              <div className="landingbtmpartners">
+                <img className="lndngpartnerdk" src={DennisKibet} alt="" />
+                <img className="lndngpartnerTenAfric" src={TenAfric} alt="" />
               </div>
             </div>
           </div>
