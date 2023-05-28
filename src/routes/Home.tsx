bdc6465c7b4c869d41c9b84032493a6dd528434e
DennisRono <finnneron1@gmail.com>
2023-05-28 17:11:13 +0300

    fixed smooth scrolling to newsletter

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react'
 import Landing from '../components/Landing'
 import Footer from '../components/Footer'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
     <Fragment>
-        <div className="homepage">
-            <Landing />
-        </div>
-        <Footer />
+      <div className="homepage">
+        <Landing />
+      </div>
+      <Newsletter />
+      <Footer />
     </Fragment>
   )
 }
