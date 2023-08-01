bdd164c33bb57e9c1e90fe9a43bc463d77d6bbad
DennisRono <dennisrkibet@gmail.com>
2023-08-01 08:48:21 +0300

    worked on the workspace sidebar and header

--- src/modules/workspace/components/Header.tsx
+++ src/modules/workspace/components/Header.tsx
@@ -1,10 +1,21 @@
 import React, { Fragment } from 'react'
 import '../styles/css/header.css'
+import { ReactComponent as Logout } from '../assets/svg/right-from-bracket-solid.svg'
 
 const Header = () => {
   return (
     <Fragment>
-      <div className="header"></div>
+      <div className="wksheader">
+        <div className="wksContainer">
+          <div className="wksWrapper">
+            <div className="wksLeft"></div>
+            <div className="wksRight">
+              <div className="wksHProfile"></div>
+              <Logout className="wksLogoutIc" />
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   )
 }
--- src/modules/workspace/components/Sidebar.tsx
+++ src/modules/workspace/components/Sidebar.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react'
 import '../styles/css/sidebar.css'
+import { ReactComponent as Angles } from '../assets/svg/angles-right-solid.svg'
 
 const Sidebar = () => {
   return (
     <Fragment>
       <div className="sidebar">
-        <h1>Sidebar</h1>
+        <Angles className="wkspaceAngles" />
       </div>
     </Fragment>
   )
