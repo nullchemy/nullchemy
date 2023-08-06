a3a6fd6255cc1bcf41c4c92fbb61448c01009ac9
DennisRono <dennisrkibet@gmail.com>
2023-08-06 12:43:09 +0300

    worked on homepage analytics

--- src/modules/workspace/routes/Home.tsx
+++ src/modules/workspace/routes/Home.tsx
@@ -5,6 +5,7 @@ import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Playarea from '../components/Playarea'
 import Footer from '../components/Footer'
+import { Helmet } from 'react-helmet'
 
 const Home = () => {
   useEffect(() => {
@@ -12,6 +13,18 @@ const Home = () => {
   }, [])
   return (
     <Fragment>
+      <Helmet>
+        <title>About Us | nullchemy</title>
+        <link
+          rel="canonical"
+          href={window.location.pathname + window.location.search}
+        />
+        <script>
+          window.dataLayer = window.dataLayer || []; function
+          gtag()&#123;dataLayer.push(arguments);&#125; gtag('js', new Date());
+          gtag('config', 'G-JXF0YL9XMJ');
+        </script>
+      </Helmet>
       <div className="wsHome">
         <Layout
           children={{
