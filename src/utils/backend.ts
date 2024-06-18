2cd3676c8e94a67cea10707e237f6216da55aee3
DennisRono <dennisrkibet@gmail.com>
2024-06-18 09:30:12 +0300

    blog empty fix bug

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -1,7 +1,7 @@
 export const backend = (): string => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    //return 'http://localhost:5000/'
-    return 'https://server.nullchemy.com/'
+    return 'http://localhost:5000/'
+    // return 'https://server.nullchemy.com/'
   } else {
     // return 'https://nullchemy-api.onrender.com/'
     return 'https://server.nullchemy.com/'
