6e644da23eae2f6038eb87fff432b6ea67b20536
DennisRono <dennisrkibet@gmail.com>
2024-07-07 15:45:14 +0300

    reverted back to using render service

--- src/utils/backend.ts
+++ src/utils/backend.ts
@@ -1,9 +1,10 @@
 export const backend = (): string => {
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     //return 'http://localhost:5000/'
-    return 'https://server.nullchemy.com/'
+    // return 'https://server.nullchemy.com/'
+    return 'https://nullchemy-api.onrender.com/'
   } else {
-    // return 'https://nullchemy-api.onrender.com/'
-    return 'https://server.nullchemy.com/'
+    return 'https://nullchemy-api.onrender.com/'
+    // return 'https://server.nullchemy.com/'
   }
 }
